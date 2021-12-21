36062f7e50b628ead0993865fa4219f10170652f
Andy <arukmoktar@gmail.com>
2021-12-21 17:55:25 +0200

    Search and SaveStateFlow

--- app/src/main/java/com/example/fulljson10/retrofit/RetrofitServieces.kt
+++ app/src/main/java/com/example/fulljson10/retrofit/RetrofitServieces.kt
@@ -6,14 +6,14 @@ import com.example.fulljson10.model.FilmResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 interface  RetrofitServieces {
-    @GET("Top250Movies/k_snggbqyh")
+    @GET("Top250Movies/k_1liv8krd")
 //    k_1liv8krd Мой
 //    k_ft56zq4q Avenger
 //    k_snggbqyh Zebra
 
     suspend fun getMovieList (): FilmResponse
 
-    @GET("Title/k_snggbqyh/{id}/Images")
+    @GET("Title/k_1liv8krd/{id}/Images")
     suspend fun getTitleList (@Path("id") id: String): TitleData
 
 
